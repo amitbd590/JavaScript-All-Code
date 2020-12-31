04c3508992cb0cde408ebbf68761dc131d3eb702
Amit <amitbd590@gmail.com>
2020-12-31 14:44:43 +0600

    Some edit.

---
+++ javascript.js
@@ -0,0 +1,86 @@
+// Declare Variable =
+
+var name = "Amit";
+var age = 27;
+var age1 = 27.2;
+
+
+// console.log() output =
+console.log(name + ' This is variable');
+console.log(age + ' This is variable');
+console.log(age1 + ' This is variable');
+
+--------------------------------------------------------
+
+Data type = 
+
+ Data type are 2 type = 
+1 = Primitive type data.
+
+-------------------------------------------------------
+
+
+Primitive ডাটা টাইপ সম্পর্কে সকল তথ্য নিচে দেয়া হল : -
+
+
+-> Number.
+
+  Number = 
+
+         number type data is ....  
+             var number = 35;
+         Float type data is ....
+            var float = 35.36;
+--------------------------------------------------------
+
+-> String.
+
+     String call =
+          var str = "string";
+          var str = 'string';
+          var str = `String`;
+          var str = string('string name');
+          var str = string(130);
+          var str = string(130.25);
+
+--------------------------------------------------------
+
+
+-> Boolean.
+
+        Boolean =
+
+        Boolean মেথড এর মাধ্যমে সত্য ও মিথ্যা প্রদান করবে
+
+         Boolean have 2 condition =
+                 1 = True;
+                 2 = False;
+
+----------------------------------------------------------
+
+
+-> Undefined.
+
+             Undefined = 
+
+        যখন ইনপুটে কোন ধরনের ভ্যালু প্রদান করা হয় না ঠিক সেই মুহুর্তে আনডিফাইন্ড দেখা দিবে
+
+----------------------------------------------------------
+
+-> Null.
+
+             Null =
+
+        যখন ভেরিয়েবলের শর্ত বা ইনপুট কিছু দিতে হয় যা অজানা থাকে তখন তা Null হিসেবে গণ্য করা হয়
+
+----------------------------------------------------------
+
+// 2 = Object type data.
+
+// -> Array.
+// -> Object.
+// -> Function.
+
+
+
+// Primitive Data Type = 
